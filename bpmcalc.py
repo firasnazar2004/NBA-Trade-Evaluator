fd3821af59de5fe523f7998f74d7b9d805174093
firasnazar2004 <22703822@emu.edu.tr>
2025-03-09 22:03:13 +0200

    Add player ID retrieval functionality and enhance team stats retrieval

--- bpmcalc.py
+++ bpmcalc.py
@@ -1,4 +1,26 @@
 import pandas as pd 
-from nba_api.stats.endpoints import playercareerstats
+import numpy as np
+from nba_api.stats.endpoints import PlayerCareerStats
 
-# Simplified BPM formula coefficients 
+
+
+#Simplified coefficients from ChatGpt that are based on historical BPM models
+a = 0.25
+b= 0.3 
+c= 0.35 
+d= 1
+e = 0.9
+f = 0.7
+
+def calculate_bpm(points, rebounds , assists, steals, blocks, turnovers, minutes):
+    if minutes == 0:
+        return 0
+    bpm = (a * points + 
+           b * rebounds + 
+           c * assists + 
+           d* steals +
+           e * blocks - 
+           0.7 * turnovers) / minutes *36
+    return round(bpm,2)
+
+player_stats = PlayerCareerStats
--- fv.py
+++ fv.py
@@ -1,10 +1,11 @@
 import pandas as pd 
-from nba_api.stats.endpoints import  TeamYearByYearStats
+from nba_api.stats.endpoints import  TeamYearByYearStats , CommonTeamRoster, PlayerCareerStats
 
 
 
 team_id = 1610612738  
 team_stats = TeamYearByYearStats(team_id=team_id).get_data_frames()[0]
+team_roster = CommonTeamRoster(team_id=team_id).get_data_frames()[0]
+player_stats = PlayerCareerStats(player_id=2544).get_data_frames()
 
-print('xyz')
-print(team_stats[['YEAR', 'WINS', 'LOSSES', 'WIN_PCT', 'CONF_RANK', 'NBA_FINALS_APPEARANCE', 'PTS_RANK']])
+print(player_stats)
